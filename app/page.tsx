dd0c921f616b4b9ddfd5585264b53e33f05af1d6
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-07-20 13:22:02 -0700

    initialize project starter

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default () => (
+  <section className="p-8">
+    <h1 className="text-center font-extrabold text-2xl text-foreground">
+      I'm Ready to Create!
+    </h1>
+  </section>
+);
