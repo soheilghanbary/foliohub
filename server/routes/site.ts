efbb4238b472771a3739c9a57a523c4ba4d519bf
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-07-22 09:10:04 -0700

    add search query on site route

--- server/routes/site.ts
+++ server/routes/site.ts
@@ -3,6 +3,27 @@ import { db } from "../db";
 import { getUserSession } from "../lib/auth";
 
 export const siteRoute = new Hono()
+  .get("/search/:q", async (c) => {
+    const q = c.req.param("q");
+    console.log(q);
+    const sites = await db.site.findMany({
+      where: {
+        AND: [
+          {
+            name: {
+              contains: q,
+            },
+          },
+          {
+            url: {
+              contains: q,
+            },
+          },
+        ],
+      },
+    });
+    return c.json(sites);
+  })
   .get("/", async (c) => {
     const sites = await db.site.findMany({
       include: { user: true },
