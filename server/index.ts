121d88d17fa20dca108fcccb13bbdfde799b55d6
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-07-23 12:38:12 +0330

    update screenshot method

--- server/index.ts
+++ server/index.ts
@@ -1,33 +1,33 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import puppeteer from "puppeteer";
 import { siteRoute } from "./routes/site";
 import { userRoute } from "./routes/user";
-import puppeteer from "puppeteer";
 
 export const app = new Hono();
 
 // middleware
 app.use("*", logger());
 
-app.post("/api/take-screenshot", async (c) => {
+app.post("/api/take", async (c) => {
   const { url } = await c.req.json();
-  try {
-    // Launch a new browser instance
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    // Navigate to the provided URL
-    await page.goto(url, { waitUntil: "networkidle2" });
-    // Take a screenshot
-    const screenshot = await page.screenshot({ encoding: "base64" });
-    // Close the browser
-    await browser.close();
-    // Return the screenshot as a base64 string
-    return c.json({ screenshot: screenshot });
-  } catch (error) {
-    // Handle any errors that occur during the process
-    console.error("Error taking screenshot:", error);
-    return c.json({ error: "Failed to take screenshot" }, 500);
-  }
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  // Navigate to the provided URL
+  await page.goto(url, { waitUntil: "networkidle2" });
+  // Take a screenshot
+  const screenshot = await page.screenshot({});
+  // Close the browser
+  await browser.close();
+  // Return the screenshot as a base64 string
+  const formData = new FormData();
+  formData.append(
+    "file",
+    new Blob([screenshot], { type: "image/png" }),
+    "screenshot.png"
+  );
+  console.log(formData);
+  return c.json({ screenshot: screenshot });
 });
 
 // rotues
